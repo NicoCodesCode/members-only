a94004e246c32da457b53a5e2c49607d0df46282
NicoCodesCode <nicolasrmz08@gmail.com>
2025-03-17 18:54:52 -0500

    connect to deployed database

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
-const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE } = process.env;
 
 module.exports = new Pool({
-  user: PGUSER,
-  password: PGPASSWORD,
-  host: PGHOST,
-  database: PGDATABASE,
-  port: PGPORT,
+  connectionString: `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`,
 });
