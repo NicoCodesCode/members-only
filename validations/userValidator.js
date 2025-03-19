2db2646b6a03e08607fdac804187c0af968bfc4b
NicoCodesCode <nicolasrmz08@gmail.com>
2025-03-18 19:20:50 -0500

    fix: you can now use spaces in the first name and last name fields

--- validations/userValidator.js
+++ validations/userValidator.js
@@ -7,15 +7,15 @@ exports.validateSignUp = [
     .withMessage("First name field can't be empty")
     .isLength({ min: 1, max: 20 })
     .withMessage("First name can only be up to 20 characters")
-    .isAlpha()
-    .withMessage("First name can only contain letters"),
+    .isAlpha(undefined, { ignore: " " })
+    .withMessage("First name can't contain special symbols nor numbers"),
   body("lastName")
     .trim()
     .notEmpty()
     .withMessage("Last name field can't be empty")
     .isLength({ min: 1, max: 20 })
     .withMessage("Last name can only be up to 20 characters")
-    .isAlpha()
+    .isAlpha(undefined, { ignore: " " })
     .withMessage("Last name can only contain letters"),
   body("username")
     .trim()
